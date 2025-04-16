5ba207bc71dd134da1f652a7d5829ec24415cc95
Andrew Rowatt <a.j.rowatt@massey.ac.nz>
2025-04-17 10:32:30 +1200

    Initial commit

---
+++ amd/src/multilanguagenames.js
@@ -0,0 +1,33 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * TODO describe module multilanguagenames
+ *
+ * @module     local_multilanguagenames/multilanguagenames
+ * @copyright  2025 Andrew Rowatt <A.J.Rowatt@massey.ac.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+import $ from 'jquery';
+
+export const init = () => {
+    $(function() {
+        console.log("multilanguagenames module initialized");
+        // Add your module initialization code here
+        $('#fitem_id_name').css('color', 'red');
+    }
+   );
+};
