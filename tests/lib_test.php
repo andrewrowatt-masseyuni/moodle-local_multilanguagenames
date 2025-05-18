d0e0238dbee1aca6e01268ac15acfa3e64a9e6a8
Andrew Rowatt <A.J.Rowatt@massey.ac.nz>
2025-05-18 15:22:18 +1200

    Updated github workflow parameters

---
+++ tests/lib_test.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_multilanguagenames;
+
+/**
+ * Tests for Multi-language section and activity names
+ *
+ * @package    local_multilanguagenames
+ * @category   test
+ * @copyright  2025 Andrew Rowatt <A.J.Rowatt@massey.ac.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+final class lib_test extends \advanced_testcase {
+    /**
+     * Test the get_multilanguagenames function
+     */
+    public function test_get_multilanguagenames() {
+        global $DB;
+
+        $this->resetAfterTest();
+
+
+        $this->assertEquals(1,1);
+    }
+}
