fa1c33440ec524304aef6d60185f988e49499b9b
Andrew Rowatt <a.j.rowatt@massey.ac.nz>
2025-06-27 15:00:08 +1200

    Added privacy provider.

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -14,20 +14,26 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+namespace local_multilanguagenames\privacy;
+
 /**
- * English language pack for Multi-language section and activity names
+ * Class provider
  *
  * @package    local_multilanguagenames
- * @category   string
  * @copyright  2025 Andrew Rowatt <A.J.Rowatt@massey.ac.nz>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+class provider implements
+    // This plugin does not store any personal user data.
+    \core_privacy\local\metadata\null_provider {
 
-defined('MOODLE_INTERNAL') || die();
-
-$string['pluginname'] = 'Multi-language section and activity names';
-$string['primaryname'] = 'Primary name';
-$string['privacy:metadata'] = 'This plugin only displays existing data.';
-$string['secondaryabove'] = 'Secondary (above)';
-$string['secondarybelow'] = 'Secondary (below)';
-$string['usemultilanguagenamehelp'] = 'Enables bilingual and trilingual names for sections and selected activities.';
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return  string
+     */
+    public static function get_reason(): string {
+        return 'privacy:metadata';
+    }
+}
